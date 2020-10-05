014923b910cba890f87a7cffa76e5b3951dbc2c0
SakshamJain7 <51989243+SakshamJain7@users.noreply.github.com>
2020-10-06 00:54:56 +0530

    Create Two_Sum.py

---
+++ Beginners Project/Python codes/Two_Sum.py
@@ -0,0 +1,18 @@
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
+
+#Example 1:
+
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+from itertools import combinations
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i,j]
