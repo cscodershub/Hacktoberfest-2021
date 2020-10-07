163b1e1a94154c774936fcf15dc6007dfbade6ed
Aryan Joshi <56163093+WHITEWOLF619@users.noreply.github.com>
2020-10-07 10:22:50 +0530

    bubble sort for python
    Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.

---
+++ Beginners Project/Python codes/bubble_sort.py
@@ -0,0 +1,46 @@
+def partition(arr, low, high):
+    i = (low-1)         
+    pivot = arr[high]     
+ 
+    for j in range(low, high):
+ 
+        
+        if arr[j] <= pivot:
+ 
+           
+            i = i+1
+            arr[i], arr[j] = arr[j], arr[i]
+ 
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    return (i+1)
+ 
+
+ 
+def quickSort(arr, low, high):
+    if len(arr) == 1:
+        return arr
+    if low < high:
+ 
+      
+        pi = partition(arr, low, high)
+ 
+        
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
+ 
+arr = [] 
+  
+
+g = int(input("Enter number of elements : ")) 
+  
+for i in range(0, g): 
+    ele = int(input()) 
+
+    arr.append(ele) 
+print(arr) 
+
+n = len(arr)
+quickSort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i]),
