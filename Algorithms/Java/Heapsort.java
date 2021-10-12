6c86e0dbdcd935a5f804331ab3cff11500f81caa
Kavya Agrawal <kavya.agrawal162@gmail.com>
2021-10-12 17:46:34 +0530

    Added Heapsort program in java + c99shells.php file restored

--- Algorithms/Java/Heapsort.java
+++ Algorithms/Java/Heapsort.java
@@ -1,52 +1,72 @@
 //Java Program for Heapsort
 
-#include<stdio.h>  //File declaration 
-int temp;  
+public class HeapSort {
   
-void heapify(int arr[], int size, int i)  //Heap function declaration 
-{  
-  int largest = i;    
-  int left = 2*i + 1;    
-  int right = 2*i + 2;    
-  
-  if (left < size && arr[left] >arr[largest])  
-      largest = left;  
-  
-  if (right < size && arr[right] > arr[largest])  
-    largest = right;  
-  
-  if (largest != i)  
-  {  
-    temp = arr[i];  
-    arr[i]= arr[largest];   
-    arr[largest] = temp;   
-    heapify(arr,size,largest);  
-  }  
-}  
-  
-void heapSort(int arr[], int size)  
-{  
-  int i;  
-  for (i = size / 2 - 1; i >= 0; i--)  
-  heapify(arr, size, i);  
-  for (i=size-1; i>=0; i--)  
- {  
-    temp = arr[0];  
-    arr[0]= arr[i];   
-    arr[i] = temp;  
-    heapify(arr, i, 0);  
- }  
-}  
-  
-void main()  //Main driver function
-{  
-  int arr[] = {1, 10, 2, 3, 4, 1, 2, 100, 23, 2};  
-  int i;  
-  int size = sizeof(arr)/sizeof(arr[0]);  
-  
-  heapSort(arr, size);  
-  
-  printf("The printed sorted elements are : \n");  
-  for (i=0; i<size; ++i)  
-  printf("%d\n",arr[i]);  
-}  
+  public void sort(int arr[])
+   {
+    int n = arr.length;
+
+    //Rearrange array 
+    for (int i = n / 2 - 1; i >= 0; i--)
+     {
+       heapify(arr, n, i);
+     }
+
+    //Extracting elements from heap one by one
+    for (int i = n - 1; i >= 0; i--) 
+    {
+      //Current root moved to the end
+      int tmp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = tmp;
+
+      heapify(arr, i, 0);           //calling max heapify on the heap reduced
+    }
+   }
+
+  void heapify(int arr[], int n, int i) {
+    int max = i;                        //Initialize max as root
+    int leftChild = 2 * i + 1;
+    int rightChild = 2 * i + 2;
+
+    //If left child is greater than root
+    if (leftChild < n && arr[leftChild] > arr[max])
+      max = leftChild;
+
+    //If right child is greater than max
+    if (rightChild < n && arr[rightChild] > arr[max])
+      max = rightChild;
+
+    //If max is not root
+    if (max != i) 
+    {
+      int swap = arr[i];
+      arr[i] = arr[max];
+      arr[max] = swap;
+
+      //heapify the affected sub-tree recursively
+      heapify(arr, n, max);
+    }
+  }
+
+  //print size of array n using utility function
+  static void display(int arr[]) {
+    int n = arr.length;
+    for (int i = 0; i < n; ++i)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+  }
+
+  //Driver code
+  public static void main(String args[]) {
+    int arr[] = {11, 34, 9, 5, 16, 10};
+
+    HeapSort hs = new HeapSort();
+    System.out.println("Original array:");
+    display(arr);
+    hs.sort(arr);
+
+    System.out.println("Sorted array:");
+    display(arr);
+  }
+}
